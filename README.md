66877b24980f3f4afd9b0c87d440adc86820d598
cromewar <vasiliy@cromewar.com>
2022-02-22 21:28:49 -0500

    finishin the project

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Monsters Cromedex
 The monster finder!
+
+## Description
+
+This is a simple project using react and [robo hash](https://robohash.org) API.
+Implements a real time finder as well
+
+## Usage
+
+1. Install the dependencies with
+
+`yarn install`
+
+2. Run the code with
+
+`yarn start`
+
+3. Have fun!
+
+![Hero Image]("./../image/hero.png "Hero Image")
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/searchbox/searchbox.component";
 
 class App extends Component {
   constructor() {
@@ -19,19 +20,24 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <input
-          type="search"
+        <h1> Monsters Cromedex</h1>
+        <SearchBox
           placeholder="search monsters"
-          onChange={(e) =>
-            this.setState({ searchField: e.target.value }, () =>
-              console.log(this.state)
-            )
-          }
+          handleChange={this.handleChange}
         />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
